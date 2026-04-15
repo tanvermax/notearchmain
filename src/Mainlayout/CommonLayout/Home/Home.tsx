e6de360619abad8412b70df24336f458b6635dd1
Tanver Mahidi <tanveermahidi@gmail.com>
2026-04-15 16:45:25 +0600

    featured section added

--- src/Mainlayout/CommonLayout/Home/Home.tsx
+++ src/Mainlayout/CommonLayout/Home/Home.tsx
@@ -19,7 +19,7 @@ export default function Home() {
        <RegisteredInstitutes/>
        <ProductShowcase/>
        <ServicesSection/>
-       {/* <FeaturesGrid/> */}
+       <FeaturesGrid/>
     </div>
   );
 }
